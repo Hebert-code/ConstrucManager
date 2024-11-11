87fd5e2fb241f77ae46ffda5ed220ba9be589435
Gustavo Souza <gustavosouza1688@gmail.com>
2024-11-10 21:00:13 -0300

    feat: add nova_categoria url to the constructmanager app

--- construcmanager/urls.py
+++ construcmanager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from construcmanager.views import index, cadastro_clientes, cadastro_fornecedores
-from construcmanager.views import cadastro_produtos, vendas, compras, estoque, controle_vendas
+from construcmanager.views import cadastro_produtos, vendas, compras, estoque, controle_vendas, nova_categoria
 urlpatterns = [
     path('', index, name='index'),
     path('cadastro_clientes', cadastro_clientes, name='cadastro_clientes'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('vendas/', vendas, name='vendas'),
     path('compras/', compras, name='compras'),
     path('estoque/', estoque, name='estoque'),
-    path('controle_vendas/', controle_vendas, name='controle_vendas')
+    path('controle_vendas/', controle_vendas, name='controle_vendas'),
+    path('nova_categoria/', nova_categoria, name='nova_categoria')
 ]
