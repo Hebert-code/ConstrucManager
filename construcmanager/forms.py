ac843cae2530fc4377ad4371eaf544aa29b6a3dc
Hebert-code <hebert.hb.brito@gmail.com>
2024-12-08 22:12:02 -0300

    Funcionalidades

--- construcmanager/forms.py
+++ construcmanager/forms.py
@@ -31,13 +31,13 @@ class ClienteForm(forms.ModelForm):
 
     def clean_cliente_cnpj(self):
         cnpj = self.cleaned_data.get('cliente_cnpj')
-        if cnpj and len(cnpj) != 14:  # Somente validação se o CNPJ for informado
+        if cnpj and len(cnpj) != 14:  
             raise forms.ValidationError('O CNPJ deve ter 14 caracteres.')
         return cnpj
 
     def clean_cliente_cpf(self):
         cpf = self.cleaned_data.get('cliente_cpf')
-        if cpf and len(cpf) != 11:  # Somente validação se o CPF for informado
+        if cpf and len(cpf) != 11:
             raise forms.ValidationError('O CPF deve ter 11 caracteres.')
         return cpf
     
@@ -62,4 +62,159 @@ class ProdutoForm(forms.ModelForm):
             'produto_quantidade_em_estoque': forms.NumberInput(attrs={'class': 'form-control'}),
             'produto_codigo': forms.NumberInput(attrs={'class': 'form-control'}),
             'produto_ponto_reposicao': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['categoria_nome']
+        widgets = {
+            'categoria_nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome da Categoria'}),
+        }
+
+class FornecedorForm(forms.ModelForm):
+    class Meta:
+        model = Fornecedor
+        fields = [
+            'fornecedor_nome',
+            'fornecedor_cnpj',
+            'fornecedor_cpf',
+            'fornecedor_email',
+            'endereco_numero',
+            'endereco_rua',
+            'endereco_bairro',
+            'endereco_cidade',
+            'endereco_estado',
+        ]
+        widgets = {
+            'fornecedor_nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome do Fornecedor'}),
+            'fornecedor_cnpj': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CNPJ'}),
+            'fornecedor_cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPF'}),
+            'fornecedor_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'endereco_numero': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Número'}),
+            'endereco_rua': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Rua'}),
+            'endereco_bairro': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Bairro'}),
+            'endereco_cidade': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cidade'}),
+            'endereco_estado': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Estado'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cnpj = cleaned_data.get('fornecedor_cnpj')
+        cpf = cleaned_data.get('fornecedor_cpf')
+        if not cnpj and not cpf:
+            raise forms.ValidationError("Informe pelo menos o CNPJ ou o CPF.")
+        if cnpj and cpf:
+            raise forms.ValidationError("Informe apenas o CNPJ ou o CPF, não ambos.")
+        return cleaned_data
+
+class VendedorForm(forms.ModelForm):
+    class Meta:
+        model = Vendedor
+        fields = [
+            'vendedor_nome',
+            'vendedor_cpf',
+        ]
+        widgets = {
+            'vendedor_nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome do Vendedor'}),
+            'vendedor_cpf' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPF'}),
+        }
+
+class VendaForm(forms.ModelForm):
+    class Meta:
+        model = Venda
+        fields = [
+            'cliente',
+            'vendedor',
+            'venda_data',
+            'venda_valor_total',
+            'venda_local_entrega',
+            'venda_status',
+            'venda_pagamento_tipo',
+            'venda_pagamento_quantidade_parcelas',
+            'venda_data_pagamento',
+        ]
+        widgets = {
+            'cliente': forms.Select(attrs={'class': 'form-control'}),
+            'vendedor': forms.Select(attrs={'class': 'form-control'}),
+            'venda_data': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'venda_valor_total': forms.NumberInput(attrs={'class': 'form-control'}),
+            'venda_local_entrega': forms.TextInput(attrs={'class': 'form-control'}),
+            'venda_status': forms.Select(attrs={'class': 'form-control'}),
+            'venda_pagamento_tipo': forms.Select(attrs={'class': 'form-control'}),
+            'venda_pagamento_quantidade_parcelas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'venda_data_pagamento': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
+
+class ProdutoVenda(forms.ModelForm):
+    class Meta:
+        model = ProdutoVenda
+        fields = [
+            'produto',
+            'venda',
+            'quantidade_produto',
+        ]
+        widgets = {
+            'produto': forms.Select(attrs={'class': 'form-control'}),
+            'venda': forms.Select(attrs={'class': 'form-control'}),
+            'quantidade_produto': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+class ProdutoCompraForm(forms.ModelForm):
+    class Meta:
+        model = ProdutoCompra
+        fields = [
+            'produto',
+            'compra',
+            'quantidade_produto',
+        ]
+        widgets = {
+            'produto': forms.Select(attrs={'class': 'form-control'}),
+            'compra': forms.Select(attrs={'class': 'form-control'}),
+            'quantidade_produto': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def save(self, commit=True):
+        produto_compra = super().save(commit=False)
+        produto = produto_compra.produto
+        quantidade = produto_compra.quantidade_produto
+
+        # Atualiza o estoque do produto
+        produto.produto_estoque += quantidade
+        produto.save()
+
+        if commit:
+            produto_compra.save()
+        return produto_compra
+
+
+class CompraForm(forms.ModelForm):
+    class Meta:
+        model = Compra
+        fields = [
+            'fornecedor',
+            'compra_data',
+            'compra_valor_total',
+            'compra_status',
+            'compra_pagamento_tipo',
+            'compra_pagamento_quantidade_parcelas',
+            'compra_pagamento_data', 
+        ]
+        widgets = {
+            'fornecedor': forms.Select(attrs={'class': 'form-control'}),
+            'compra_data': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'compra_valor_total': forms.NumberInput(attrs={'class': 'form-control'}),
+            'compra_status': forms.Select(attrs={'class': 'form-control'}),
+            'compra_pagamento_tipo': forms.Select(attrs={'class': 'form-control'}),
+            'compra_pagamento_quantidade_parcelas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'compra_pagamento_data': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
+
+class AtualizarEstoqueForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['produto_nome', 'produto_quantidade_em_estoque']
+        widgets = {
+            'produto_nome': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
+            'produto_quantidade_em_estoque': forms.NumberInput(attrs={'class': 'form-control'}),
         }
--- construcmanager/views.py
+++ construcmanager/views.py
@@ -1,44 +1,356 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Cliente, Produto, Fornecedor, Venda, Compra
+from .forms import ClienteForm, ProdutoForm, FornecedorForm, VendaForm, CompraForm, ProdutoCompraForm, AtualizarEstoqueForm
+
+
+from django.forms import formset_factory
+from django.db import transaction
+
+import csv
+from datetime import datetime
+from django.db.models import Sum
 from django.http import HttpResponse
-from .models import Produto, Categoria, Fornecedor, Venda, Compra
-from .forms import ClienteForm, ProdutoForm
+from xhtml2pdf import pisa
+from django.template.loader import render_to_string
 
-def index(request):
-    return render(request, 'construcmanager/index.html') 
+#Clientes
 
-def cadastro_produtos(request):
-    
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('pagina_sucesso')  # Substitua por uma URL apropriada
-    else:
-        form = ProdutoForm()
-    return render(request, 'construcmanager/cadastro_produtos.html', {'form': form})
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'construcmanager/clientes/listar.html', {'clientes': clientes})
 
-def cadastro_fornecedores(request):
-    return render(request, 'construcmanager/cadastro_fornecedores.html') 
-
-def cadastro_clientes(request):
+def criar_cliente(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_clientes') 
+            return redirect('listar_clientes')
     else:
         form = ClienteForm()
+    return render(request, 'construcmanager/clientes/form.html', {'form': form})
 
-    return render(request, 'construcmanager/cadastro_clientes.html', {'form': form})
+def atualizar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'construcmanager/clientes/form.html', {'form': form})
 
-def vendas(request):
-    return render(request, 'construcmanager/vendas.html') 
+def deletar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('listar_clientes')
+    return render(request, 'construcmanager/clientes/confirmar_exclusao.html', {'cliente': cliente})
 
-def compras(request):
-    return render(request, 'construcmanager/compras.html') 
+#Clientes
 
-def estoque(request):
-    return render(request, 'construcmanager/estoque.html') 
 
-def controle_vendas(request):
-    return render(request, 'construcmanager/controle_vendas.html') 
+#Produtos
+
+def listar_produtos(request):
+    produto = Produto.objects.all()
+    return render(request, 'construcmanager/produto/listar.html', {'produto': produto})
+
+def cadastro_produtos(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_produtos')
+    else:
+        form = ProdutoForm()
+    
+    produtos = Produto.objects.all()
+    return render(request, 'construcmanager/produto/cadastro_produtos.html', {'form': form, 'produtos': produtos})
+
+def atualizar_produto(request, pk):
+    produto = get_object_or_404(Produto, pk=pk)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'construcmanager/produto/editar_produto.html', {'form': form, 'produto': produto})
+
+def delete_produto(request, pk):
+    produto = get_object_or_404(Produto, pk=pk)
+    if request.method == 'POST':
+        produto.delete() 
+        return redirect('listar_produtos')
+    return render(request, 'construcmanager/produto/excluir_produto.html', {'produto': produto})
+
+#Produtos
+
+
+
+#Fornecedores
+
+def listar_fornecedor(request):
+    fornecedor = Fornecedor.objects.all()
+    return render(request, 'construcmanager/fornecedor/listar_fornecedor.html', {'fornecedores': fornecedor})
+
+def cadastro_fornecedor(request):
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_fornecedor')
+    else:
+        form = FornecedorForm
+    return render(request, 'construcmanager/fornecedor/cadastro_fornecedor.html', {'form': form})
+
+def atualizar_fornecedor(request, pk):
+    fornecedor = get_object_or_404(Fornecedor, pk=pk)
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST, instance=fornecedor)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_fornecedor')
+    else:
+        form = FornecedorForm(instance=fornecedor)
+    return render(request, 'construcmanager/fornecedor/atualizar_fornecedor.html', {'form': form, 'fornecedor': fornecedor})
+
+def deletar_fornecedor(request, pk):
+    fornecedor = get_object_or_404(Fornecedor, pk=pk)
+    if request.method == 'POST':
+        fornecedor.delete()
+        return redirect('listar_fornecedor')
+    return render(request, 'construcmanager/fornecedor/deletar_fornecedor.html', {'fornecedor': fornecedor})
+
+#Fornecedores
+
+
+
+#Vendas
+
+def listar_vendas(request):
+    vendas = Venda.objects.all()
+    return render(request, 'construcmanager/vendas/listar_vendas.html', {'vendas': vendas})
+
+def nova_venda(request):
+    if request.method == 'POST':
+        form = VendaForm(request.POST)
+        if form.is_valid():
+            venda = form.save(commit=False)  # Salva a venda sem enviar ao banco ainda
+            produto = venda.produto  # Acessa o produto relacionado
+
+            if produto.quantidade >= venda.quantidade:  # Verifica se há estoque suficiente
+                try:
+                    with transaction.atomic():  # Garante que as operações são atômicas
+                        produto.quantidade -= venda.quantidade  # Atualiza o estoque
+                        produto.save()  # Salva a atualização do produto
+                        venda.save()  # Salva a venda no banco
+                    return redirect('listar_vendas')
+                except Exception as e:
+                    # Captura qualquer erro durante o salvamento e adiciona ao formulário
+                    form.add_error(None, f"Ocorreu um erro ao salvar a venda: {str(e)}")
+            else:
+                # Adiciona um erro ao formulário se o estoque for insuficiente
+                form.add_error('quantidade', 'Estoque insuficiente para realizar a venda.')
+    else:
+        form = VendaForm()
+    
+    return render(request, 'construcmanager/vendas/nova_venda.html', {'form': form})
+
+
+def editar_venda(request, pk): 
+    venda = get_object_or_404(Venda, pk=pk)
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST,instance=venda)
+        if form.is_valid:
+            form.save()
+            return redirect('listar_vendas')
+    else:
+        form = VendaForm(instance=venda)
+    return(request, 'construcmanager/vendas/editar_vendas.html', {'form': form})
+
+def deletar_venda(request, pk):
+    venda = get_object_or_404(Venda, pk=pk)
+    if request.method == 'POST':
+        venda.delete()
+        return redirect('listar_vendas')
+    return render(request, 'construcmanager/vendas/deletar_venda.html', {'venda': venda})
+
+#Vendas
+
+
+#Compras
+
+def listar_compras(request):
+    compras = Compra.objects.all() 
+    return render(request, 'construcmanager/compra/listar_compras.html', {'compras': compras})
+
+def nova_compra(request):
+    ProdutoCompraFormSet = formset_factory(ProdutoCompraForm, extra=1)
+
+    if request.method == 'POST':
+        compra_form = CompraForm(request.POST)
+        formset = ProdutoCompraFormSet(request.POST)
+
+        if compra_form.is_valid() and formset.is_valid():
+            with transaction.atomic():  # Garante consistência dos dados
+                compra = compra_form.save()
+                for form in formset:
+                    produto_compra = form.save(commit=False)
+                    produto_compra.compra = compra  # Associa o produto à compra
+                    produto_compra.save()
+            return redirect('listar_compras')
+    else:
+        compra_form = CompraForm()
+        formset = ProdutoCompraFormSet()
+
+    return render(request, 'construcmanager/compra/nova_compra.html', {
+        'compra_form': compra_form,
+        'formset': formset,
+    })
+
+def detalhar_compra(request, compra_id):
+    compra = get_object_or_404(Compra, id=compra_id) 
+    return render(request, 'construcmanager/compra/detalhar_compra.html', {'compra': compra})
+
+def cancelar_compra(request, compra_id):
+    compra = get_object_or_404(Compra, id=compra_id)
+    compra.compra_status = 'CANCELADO' 
+    compra.save()
+    return redirect('listar_compras') 
+
+#Compras 
+
+#Estoque
+
+def consultar_estoque(request):
+    produtos = Produto.objects.all()
+    return render(request, 'construcmanager/estoque/consultar_estoque.html', {'produtos': produtos})
+
+def atualizar_estoque(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+    if request.method == 'POST':
+        form = AtualizarEstoqueForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('consultar_estoque')
+    else:
+        form = AtualizarEstoqueForm(instance=produto)
+    return render(request, 'construcmanager/estoque/atualizar_estoque.html', {'form': form})
+
+def alertas_estoque(request):
+    produtos = Produto.objects.filter(produto_estoque__lt=10) 
+    return render(request, 'construcmanager/estoque/alertas_estoque.html', {'produtos': produtos})
+
+#Estoque
+
+#Relatorio
+
+# Relatório de Vendas
+
+# Página central de relatórios
+def pagina_relatorios(request):
+    return render(request, 'construcmanager/relatorios/pagina_relatorios.html')
+
+# Relatório de Vendas
+def relatorio_vendas(request):
+    data_inicial = request.GET.get('data_inicial')
+    data_final = request.GET.get('data_final')
+    vendas = Venda.objects.all()
+
+    if data_inicial and data_final:
+        vendas = vendas.filter(data__range=[data_inicial, data_final])
+    
+    total_vendas = vendas.aggregate(Sum('valor_total'))['valor_total__sum'] or 0
+
+    return render(request, 'construcmanager/relatorios/relatorio_vendas.html', {
+        'vendas': vendas,
+        'total_vendas': total_vendas,
+        'data_inicial': data_inicial,
+        'data_final': data_final,
+    })
+
+def exportar_vendas_csv(request):
+    data_inicial = request.GET.get('data_inicial')
+    data_final = request.GET.get('data_final')
+    vendas = Venda.objects.all()
+
+    if data_inicial and data_final:
+        vendas = vendas.filter(data__range=[data_inicial, data_final])
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="vendas.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Cliente', 'Data', 'Valor Total'])
+    for venda in vendas:
+        writer.writerow([venda.id, venda.cliente, venda.data, venda.valor_total])
+
+    return response
+
+def exportar_vendas_pdf(request):
+    data_inicial = request.GET.get('data_inicial')
+    data_final = request.GET.get('data_final')
+    vendas = Venda.objects.all()
+
+    if data_inicial and data_final:
+        vendas = vendas.filter(data__range=[data_inicial, data_final])
+
+    context = {'vendas': vendas, 'data_inicial': data_inicial, 'data_final': data_final}
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="vendas.pdf"'
+    html = render_to_string('construcmanager/relatorios/relatorio_vendas_pdf.html', context)
+    pisa.CreatePDF(html, dest=response)
+    return response
+
+# Relatório de Compras
+def relatorio_compras(request):
+    data_inicial = request.GET.get('data_inicial')
+    data_final = request.GET.get('data_final')
+    compras = Compra.objects.all()
+
+    if data_inicial and data_final:
+        compras = compras.filter(data__range=[data_inicial, data_final])
+
+    total_compras = compras.aggregate(Sum('valor_total'))['valor_total__sum'] or 0
+
+    return render(request, 'construcmanager/relatorios/relatorio_compras.html', {
+        'compras': compras,
+        'total_compras': total_compras,
+        'data_inicial': data_inicial,
+        'data_final': data_final,
+    })
+
+def exportar_compras_csv(request):
+    compras = Compra.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="compras.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Fornecedor', 'Data', 'Valor Total'])
+    for compra in compras:
+        writer.writerow([compra.id, compra.fornecedor, compra.data, compra.valor_total])
+
+    return response
+
+# Relatório de Estoque
+def relatorio_estoque(request):
+    produtos = Produto.objects.all()
+    return render(request, 'construcmanager/relatorios/relatorio_estoque.html', {'produtos': produtos})
+
+def exportar_estoque_csv(request):
+    produtos = Produto.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="estoque.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Nome', 'Quantidade', 'Preço'])
+    for produto in produtos:
+        writer.writerow([produto.id, produto.nome, produto.quantidade, produto.preco])
+
+    return response
+
+#Relatorio
