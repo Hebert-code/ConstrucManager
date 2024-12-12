08326454fbd237aa6a640565944d304af0029217
Gustavo Souza <gustavosouza1688@gmail.com>
2024-12-12 09:10:04 -0300

    refactor: make so the default path redirects to login page

--- setup/urls.py
+++ setup/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/accounts/login', permanent=False)),
     path('admin/', admin.site.urls),
     path('construcmanager/', include('construcmanager.urls')),
     path('accounts/', include('allauth.urls')),
